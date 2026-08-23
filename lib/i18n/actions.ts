6a5345e7a89789cfcff84beddcbf71fb3dd344d1
Filiberto Sañudo <filibertosanudo@gmail.com>
2026-08-23 04:29:56 -0700

    Add Server Action to set the admin language cookie

---
+++ lib/i18n/actions.ts
@@ -0,0 +1,16 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { ADMIN_LANG_COOKIE } from "./cookie";
+import type { Lang } from "./lang";
+
+const ONE_YEAR = 60 * 60 * 24 * 365;
+
+export async function setAdminLangAction(lang: Lang) {
+  const cookieStore = await cookies();
+  cookieStore.set(ADMIN_LANG_COOKIE, lang, {
+    path: "/",
+    maxAge: ONE_YEAR,
+    sameSite: "lax",
+  });
+}
